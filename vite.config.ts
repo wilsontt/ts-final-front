a021c2cd16e0f8e33671eba80124947eb51f1d29
wilsontt <wilsontt@icloud.com>
2025-12-19 18:49:15 +0800

    建立 deploy 及 修改 vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { fileURLToPath, resolve, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url'
 
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
@@ -6,23 +6,24 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/2025-ts-task-2/',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  build: {
-    target: 'esnext',
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-      },
-      output: {
-        dir: resolve(__dirname, 'dist'),
-      },
-    },
-  },
+  // build: {
+  //   target: 'esnext',
+  //   rollupOptions: {
+  //     input: {
+  //       main: resolve(__dirname, 'index.html'),
+  //     },
+  //     output: {
+  //       dir: resolve(__dirname, 'dist'),
+  //     },
+  //   },
+  // },
   css: {
     preprocessorOptions: {
       scss: {
